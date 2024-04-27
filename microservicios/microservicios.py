86ef9389b4f7ff8e0c002d4abbc236da8a0d7671
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-27 16:36:25 -0400

    [Esteban Arenas] Implementación solución con microservicios haciendo uso de libreria MPI

---
+++ microservicios/microservicios.py
@@ -0,0 +1,53 @@
+from mpi4py import MPI
+
+def leer_archivo(archivo):
+    datos = []
+    with open(archivo, 'r') as file:
+        for linea in file:
+            estacion, temperatura = linea.split(';')
+            datos.append((estacion, float(temperatura)))
+    return datos
+
+def calcular_temperaturas(datos):
+    estaciones = []
+    resultados = []
+
+    for estacion, temperatura in datos:
+        if estacion not in estaciones:
+            estaciones.append(estacion)
+
+            temp_min = temperatura
+            temp_max = temperatura
+            temp_total = 0
+            temp_contador = 0
+
+            for estacion_aux, temp_aux in datos:
+                if estacion_aux == estacion:
+                    temp_min = min(temp_min, temp_aux)
+                    temp_max = max(temp_max, temp_aux)
+                    temp_total += temp_aux
+                    temp_contador += 1
+
+            temp_promedio = temp_total / temp_contador
+            resultados.append((estacion, temp_min, temp_max, temp_promedio))
+            
+    return resultados
+
+def guardar_resultados(resultados, archivo_salida):
+    with open(archivo_salida, 'w') as file:
+        file.write("Estacion;Temp. Minima;Temp. Maxima;Temp. Promedio\n")
+        for estacion, temp_min, temp_max, temp_promedio in resultados:
+            file.write(f"{estacion};{temp_min};{temp_max};{temp_promedio:.1f}\n")
+
+if __name__ == "__main__":
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    if rank == 0:
+        archivo_entrada = "archivos/archivo-entrada-20.txt"
+        datos = leer_archivo(archivo_entrada)
+        comm.send(datos, dest=1)
+    else:
+        datos = comm.recv(source=0)  # Recibimos los datos
+        resultados = calcular_temperaturas(datos)
+        guardar_resultados(resultados, "archivos/archivo-salida-microservicios.txt")
